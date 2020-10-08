243f961a04f0da928e39ce9fc4b22f70357b4bae
Pashs-ba <pashs.ba@gmail.com>
2020-10-08 16:13:40 +0300

    IMPROVEMENTS

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ config = configparser.ConfigParser()
 config.read('settings.ini')
 token = config['MAIN']['token']
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    pass
+bot = telebot.TeleBot(token)
 
+
+@bot.message_handler()
+def handle_start_help(message):
+
+    admin = {'ivan2832': 0,
+             'Pashs_ba': 370666658}
+    if message.from_user.username in admin:
+        bot.send_message(message.chat.id, 'Будущая админка')
+        print(message.chat.id, message.from_user.username)
+    else:
+        bot.send_message(message.chat.id, 'Не для тебя холоп')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
