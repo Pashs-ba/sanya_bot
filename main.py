0202241e7f87d8f22a199794780c2f02efec9d95
cogito_ergo_sum <alex1205323alex@gmail.com>
2020-10-08 17:22:37 +0300

    Commit

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import telebot
 import configparser
+from random import randint
 
 config = configparser.ConfigParser()
 config.read('settings.ini')
 token = config['MAIN']['token']
 
 bot = telebot.TeleBot(token)
-
-
+ans = {'Ня! Саня сказал, что тебя не было на занятиях, ууу, а если я разозлюсь?',
+       'Эээ, а кодить? Вы и так вымираете, а ты не ходишь',
+       'Эй! Ты чего меня обижаешь? Почему тебя не наблюдалось занятии?',
+       'Здрасте, а чего это мы не кодим? Я ведь знаю, что тебя не было! Я всё знаю!'}
 @bot.message_handler()
 def main(message):
     admin = {'ivan2832': 1061564807,
@@ -16,7 +19,7 @@ def main(message):
         bot.send_message(message.chat.id, 'Будущая админка')
         print(message.chat.id, message.from_user.username)
     else:
-        bot.send_message(message.chat.id, 'Пшел кодить')
+        bot.send_message(message.chat.id, ans[randint(0, len(ans) - 1)])
     for i in admin:
         bot.send_message(admin[i], '{} послал боту такое сообщение: {}, id {}'.format(message.from_user.username, message.text, message.chat.id))
 
