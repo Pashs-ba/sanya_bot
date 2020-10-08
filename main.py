4226c840abd0a8b60e6b3e219eb960060f1a720d
Pashs-ba <pashs.ba@gmail.com>
2020-10-08 16:22:32 +0300

    IMPROVEMENTS

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler()
 def handle_start_help(message):
-
-    admin = {'ivan2832': 0,
+    # 'ivan2832': 0,
+    admin = {'ivan2832': 1061564807,
              'Pashs_ba': 370666658}
     if message.from_user.username in admin:
         bot.send_message(message.chat.id, 'Будущая админка')
         print(message.chat.id, message.from_user.username)
     else:
-        bot.send_message(message.chat.id, 'Не для тебя холоп')
+        bot.send_message(message.chat.id, 'Пшел кодить')
+    for i in admin:
+        bot.send_message(admin[i], '{} послал боту такое сообщение: {}, id {}'.format(message.from_user.username, message.text, message.chat.id))
 
 
 if __name__ == '__main__':
